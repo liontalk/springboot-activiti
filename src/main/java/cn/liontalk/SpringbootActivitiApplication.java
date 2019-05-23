f091e44aa0084d81bab33ad6025f1b4a6e8128d2
liontalk <liontalk@163.com>
2019-05-23 10:32:29 +0800

    整合activiti项目启动完出

--- src/main/java/cn/liontalk/SpringbootActivitiApplication.java
+++ src/main/java/cn/liontalk/SpringbootActivitiApplication.java
@@ -1,13 +1,14 @@
 package cn.liontalk;
 
+import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 @MapperScan("cn.liontalk.dao")
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SpringbootActivitiApplication {
 
     private final static Logger logger = LoggerFactory.getLogger(SpringbootActivitiApplication.class);
--- src/main/java/cn/liontalk/web/DemoController.java
+++ src/main/java/cn/liontalk/web/DemoController.java
@@ -1,9 +1,13 @@
 package cn.liontalk.web;
 
-import org.activiti.bpmn.model.Task;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.repository.ProcessDefinition;
 import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,35 +17,43 @@ import org.springframework.web.bind.annotation.RestController;
  * @description DemoController
  * @date 2019-05-20 21:28
  **/
-@RequestMapping(value = "/demo")
+@RequestMapping("/demo")
 @RestController
 public class DemoController {
 
 
+    @Autowired
+    private RepositoryService repositoryService;
+
+    @Autowired
+    private RuntimeService runtimeService;
+
+    @Autowired
+    private TaskService  taskService;
 
 
-    @RequestMapping(value = "/first")
+    @RequestMapping("/first")
     public void firstDemo(){
 
-        //根据bpmn文件部署流程
-//        Deployment deployment = repositoryService.createDeployment().addClasspathResource("demo2.bpmn").deploy();
-//        //获取流程定义
-//        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery().deploymentId(deployment.getId()).singleResult();
-//        //启动流程定义，返回流程实例
-//        ProcessInstance pi = runtimeService.startProcessInstanceById(processDefinition.getId());
-//        String processId = pi.getId();
-//        System.out.println("流程创建成功，当前流程实例ID："+processId);
-//
-//        Task task=taskService.createTaskQuery().processInstanceId(processId).singleResult();
-//        System.out.println("第一次执行前，任务名称："+task.getName());
-//        taskService.complete(task.getId());
-//
-//        task = taskService.createTaskQuery().processInstanceId(processId).singleResult();
-//        System.out.println("第二次执行前，任务名称："+task.getName());
-//        taskService.complete(task.getId());
-//
-//        task = taskService.createTaskQuery().processInstanceId(processId).singleResult();
-//        System.out.println("task为null，任务执行完毕："+task);
+
+        Deployment deployment = repositoryService.createDeployment().addClasspathResource("demo2.bpmn").deploy();
+        //获取流程定义
+        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery().deploymentId(deployment.getId()).singleResult();
+        //启动流程定义，返回流程实例
+        ProcessInstance pi = runtimeService.startProcessInstanceById(processDefinition.getId());
+        String processId = pi.getId();
+        System.out.println("流程创建成功，当前流程实例ID："+processId);
+
+        Task task=taskService.createTaskQuery().processInstanceId(processId).singleResult();
+        System.out.println("第一次执行前，任务名称："+task.getName());
+        taskService.complete(task.getId());
+
+        task = taskService.createTaskQuery().processInstanceId(processId).singleResult();
+        System.out.println("第二次执行前，任务名称："+task.getName());
+        taskService.complete(task.getId());
+
+        task = taskService.createTaskQuery().processInstanceId(processId).singleResult();
+        System.out.println("task为null，任务执行完毕："+task);
 
 
     }
